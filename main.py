281ffe2c4828ce51fe57873c75781f3a4661cce8
fabriciomedeiros <fabriciomedeiros@remax.com.br>
2023-11-11 19:30:31 -0300

    testing pipeline

--- main.py
+++ main.py
@@ -8,18 +8,15 @@ def exit_script():
 
 scheduler = BackgroundScheduler()
 scheduler.start()
-def teste():
-    print("testing!!")
-print("running!!!!!")
+print("running")
 
 #chatlogs
-# scheduler.add_job(chat, 'interval', minutes=2, args=["https://www.youtube.com/@DSPGaming","dsp"])
-# scheduler.add_job(chat, 'interval', minutes=3, args=["https://www.youtube.com/@DarkDavesMirror","ddm"])
+scheduler.add_job(chat, 'interval', minutes=2, args=["https://www.youtube.com/@DSPGaming","dsp"])
+scheduler.add_job(chat, 'interval', minutes=3, args=["https://www.youtube.com/@DarkDavesMirror","ddm"])
 
 # #superchats
-# schedule.every(2).minutes.do(lambda: superchats("https://www.youtube.com/@DSPGaming","dsp") )
-# schedule.every(3).minutes.do(lambda: superchats("https://www.youtube.com/@DarkDavesMirror","ddm"))
-schedule.every(2).minutes.do(lambda: teste())
+schedule.every(2).minutes.do(lambda: superchats("https://www.youtube.com/@DSPGaming","dsp") )
+schedule.every(3).minutes.do(lambda: superchats("https://www.youtube.com/@DarkDavesMirror","ddm"))
 # schedule.every(5).minutes.do(lambda: superchats("https://www.youtube.com/@RawPhil","raw"))
 schedule.every().day.at("09:00").do(exit_script)
 try:
@@ -27,5 +24,5 @@ try:
         time.sleep(1)
         schedule.run_pending()
 except (KeyboardInterrupt, SystemExit):
-    #scheduler.shutdown()
+    scheduler.shutdown()
     schedule.clear()
