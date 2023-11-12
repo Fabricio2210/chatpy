e8df01c9ccc70ea34a90e547c11145c9166692a5
fabricio2210 <fabricio215@hotmail.com>
2023-11-11 22:21:49 -0300

    tst

--- superchat.py
+++ superchat.py
@@ -5,23 +5,24 @@ import platform
 
 superchat_locks = {}
 
-def open_new_terminal(command):
-    if platform.system() == 'Windows':
-        subprocess.Popen(['start', 'cmd', '/c', command], shell=True)
-    if platform.system() == 'Linux':
-            subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', command])
+def open_new_terminal(command, log_file):
+    with open(log_file, 'w') as log:
+        subprocess.Popen(command, stdout=log, stderr=log)
 
 def superchat(video_id, subject):
     yesterday = datetime.now()
     formatted_date = yesterday.strftime("%Y-%m-%d")
 
     shell_script = f'./{subject}/superchats/run_chat{subject}.sh'
+    log_file = f'{subject}_log.txt'
     with open(shell_script, 'w') as file:
         file.write(f'#!/bin/bash\nchat_downloader https://www.youtube.com/watch?v={video_id} --output ./{subject}/superchats/{formatted_date}_superchats.json --message_groups "superchat"')
+
     if (video_id, subject) not in superchat_locks:
-        thread = threading.Thread(target=lambda: open_new_terminal(f'bash {shell_script}'))
+        command = ['bash', shell_script]
+        thread = threading.Thread(target=lambda: open_new_terminal(command, log_file))
         thread.start()
         superchat_locks[(video_id, subject)] = threading.Lock()
         print(superchat_locks)
     else:
-        print(f"{ shell_script} already running")
+        print(f"{shell_script} already running")
