e3b7a3d0fdb7c380216d51693f43e5c46be75e9b
fabricio2210 <fabricio215@hotmail.com>
2023-11-11 22:31:41 -0300

    added all channels

--- main.py
+++ main.py
@@ -11,14 +11,24 @@ scheduler.start()
 print("running!!")
 
 #chatlogs
-scheduler.add_job(chat, 'interval', minutes=2, args=["https://www.youtube.com/@Pirulla25","dsp"])
-scheduler.add_job(chat, 'interval', minutes=3, args=["https://www.youtube.com/@DarkDavesMirror","ddm"])
-
+scheduler.add_job(chat, 'interval', minutes=10, args=["https://www.youtube.com/@meerkat_mob","meerkat"])
+scheduler.add_job(chat, 'interval', minutes=10, args=["https://www.youtube.com/@DSPGaming","dsp"])
+scheduler.add_job(chat, 'interval', minutes=9, args=["https://www.youtube.com/@DarkDavesMirror ","ddm"])
+scheduler.add_job(chat, 'interval', minutes=9, args=["https://www.youtube.com/@RawPhil","raw"])
+scheduler.add_job(chat, 'interval', minutes=20, args=["https://www.youtube.com/@DSPReacts","reacts"])
+scheduler.add_job(chat, 'interval', minutes=10, args=["https://www.youtube.com/@ShinkoFleur","shinko"])
+scheduler.add_job(chat, 'interval', minutes=20, args=["https://www.youtube.com/@AgentProper","proper"])
+scheduler.add_job(chat, 'interval', minutes=15, args=["https://www.youtube.com/@WPIG1651","wpig"])
+scheduler.add_job(chat, 'interval', minutes=15, args=["https://www.youtube.com/@ThatBeingSaid","tbs"])
+scheduler.add_job(chat, 'interval', minutes=20, args=["https://www.youtube.com/@TheDecepticron","decepticron"])
+scheduler.add_job(chat, 'interval', minutes=15, args=["https://www.youtube.com/@Aqua_Teal","aqua"])
+scheduler.add_job(chat, 'interval', minutes=14, args=["https://www.youtube.com/@detractorbeam","beam"])
+scheduler.add_job(chat, 'interval', minutes=11, args=["https://www.youtube.com/@PieceofPeace","pop"])
+scheduler.add_job(chat, 'interval', minutes=11, args=["https://www.youtube.com/@doodystreams","doody"])
 # #superchats
-schedule.every(2).minutes.do(lambda: superchats("https://www.youtube.com/@Pirulla25","dsp") )
-schedule.every(3).minutes.do(lambda: superchats("https://www.youtube.com/@DarkDavesMirror","ddm"))
-# schedule.every(5).minutes.do(lambda: superchats("https://www.youtube.com/@RawPhil","raw"))
-schedule.every().day.at("09:00").do(exit_script)
+schedule.every(2).minutes.do(lambda: superchats("https://www.youtube.com/@DSPGaming","dsp") )
+schedule.every(3).minutes.do(lambda: superchats("https://www.youtube.com/@doodystreams","doody"))
+#schedule.every().day.at("09:00").do(exit_script)
 try:
     while True:
         time.sleep(1)
