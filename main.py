c84827595ff3f022f1d2cd2232d197b9e47bcdff
Fabricio <fabricio215@hotmail.com>
2024-01-16 19:18:16 -0300

    Testing pipelines

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def exit_script():
 
 scheduler = BackgroundScheduler()
 scheduler.start()
-print("running")
+print("running!!")
 
 #chatlogs
 scheduler.add_job(chat, 'interval', minutes=10, args=["https://www.youtube.com/@meerkat_mob","meerkat"])
