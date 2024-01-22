38c398009a6e0350f8e7c332fe624aa0cc9c5811
fabriciomedeiros <fabriciomedeiros@remax.com.br>
2024-01-22 20:23:39 -0300

    Testing pipelines

--- ddm/superchats/run_chat.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-chat_downloader https://www.youtube.com/watch?v=zzVbw6XF7Io --output ./ddm/superchats/2023-11-04_superchats.json --message_groups "superchat"
--- dsp/superchats/run_chat.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-chat_downloader https://www.youtube.com/watch?v=3egEhlXHYPg --output ./dsp/superchats/2023-11-04_superchats.json --message_groups "superchat"
