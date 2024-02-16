f870999ac03a585ce311e5099021c27fbd95fede
fabriciomedeiros <fabriciomedeiros@remax.com.br>
2024-02-16 14:13:57 -0300

    Fix: fixed date at joinfiles function

--- joinFiles.py
+++ joinFiles.py
@@ -1,10 +1,13 @@
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 def joinFiles(folder_path):
-    today_date = datetime.now().strftime('%Y-%m-%d')
+    today_date = datetime.now()
+    one_day_delta = timedelta(days=1)
+    new_date = today_date - one_day_delta
+    formatted_date = new_date.strftime('%Y-%m-%d')
     folder = Path(folder_path)
-    matching_files = [file for file in folder.iterdir() if today_date in file.name and file.suffix.lower() == '.json']
+    matching_files = [file for file in folder.iterdir() if formatted_date in file.name and file.suffix.lower() == '.json']
     all_data = []
     
     for file in matching_files:
@@ -15,7 +18,7 @@ def joinFiles(folder_path):
     if len(all_data) == 0:
         print(f"There is no files today for {folder_path}")
     else:
-        output_file_name = f"{today_date}_superchats.json"
+        output_file_name = f"{formatted_date}_superchats.json"
         output_file_path = folder / output_file_name
 
         with open(output_file_path, 'w') as output_file:
